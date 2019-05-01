d9065d7d03e10d860b2ca5399f258ee307dd98cd
oleks <olekstaruch@gmail.com>
2019-05-01 19:41:52 +0200

    add actionlistener for random and clear button

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Main extends JFrame{
 
@@ -12,32 +13,30 @@ public class Main extends JFrame{
         this.add(upPanel,BorderLayout.PAGE_START);
         upPanel.add(new JLabel("Gra o życie"));
 
-        JButton beging =new JButton("Przywróc stan początkowy");upPanel.add(beging);
+        JButton random =new JButton("Losowy stan");upPanel.add(random);
         JButton clear =new JButton("Wyczyść");upPanel.add(clear);
 
 
         int w=50,h=50;
         boolean [][]tab=new boolean[w][h];
-        JButton [][]buttons = new JButton[w][h];
+        MyButton [][]buttons = new MyButton[w][h];
 
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new GridLayout(h,w));
         this.add(centerPanel,BorderLayout.CENTER);
         for(int i=0;i<w;i++){
             for(int j=0;j<h;j++){
-                Icon icon=new ImageIcon();
-                MyButton tmp = new MyButton();
+                tab[i][j]=false;
+                MyButton tmp = new MyButton(i,j);
                 centerPanel.add(tmp);
                 buttons[i][j]=tmp;
-                tab[i][j]=false;
+
+                tmp.addActionListener(e -> {
+                    tmp.setIcon();
+                    tab[tmp.w][tmp.h]=tmp.b;
+                });
             }
-
-
         }
-
-
-
-
 
         JPanel downPanel = new JPanel();
         downPanel.setLayout(new GridLayout(1,3));
@@ -46,15 +45,29 @@ public class Main extends JFrame{
         JButton button1step = new JButton("Jeden ruch");downPanel.add(button1step);
         JButton button5step = new JButton("5 ruchów");downPanel.add(button5step);
         JButton button10step = new JButton("10 ruchów");downPanel.add(button10step);
-
-
-
         JButton start = new JButton("Start");downPanel.add(start);
 
+        clear.addActionListener(e -> {
+            for(int i=0;i<w;i++){
+                for(int j=0;j<h;j++){
+                    tab[i][j]=false;
+                    buttons[i][j].b=false;
+                    buttons[i][j].setIcon();
+                }
+            }
+        });
 
-
-
-
+        random.addActionListener(e->{
+            for(int i=0;i<w;i++){
+                for(int j=0;j<h;j++){
+                    Random r = new Random();
+                    boolean tmp=r.nextBoolean();
+                    tab[i][j]=tmp;
+                    buttons[i][j].b=tmp;
+                    buttons[i][j].setIcon();
+                }
+            }
+        });
 
 
 
--- src/MyButton.java
+++ src/MyButton.java
@@ -3,14 +3,25 @@ import java.awt.*;
 
 public class MyButton extends JButton {
 
-    public MyButton(Icon icon) {
-        super(icon);
+    boolean b;
+    int w,h;
+    Icon no=new ImageIcon("no.png");
+    Icon yes=new ImageIcon("yes.png");
+
+    public MyButton(int w,int h) {
+        this.b=false;
+
+        this.setIcon(no);
         this.setPreferredSize(new Dimension(30,30));
     }
 
-
-    @Override
-    public void setIcon(Icon defaultIcon) {
-        super.setIcon(defaultIcon);
+    public void setIcon() {
+        if(!b){
+            this.setIcon(no);
+            b=true;
+        }else{
+            this.setIcon(yes);
+            b=false;
+        }
     }
 }
