33d3f76f0727ca4fd01b42233472fea7d49124ec
oleks <olekstaruch@gmail.com>
2019-05-03 19:01:14 +0200

    update

--- src/zad1/MyButton.java
+++ src/zad1/MyButton.java
@@ -7,23 +7,21 @@ public class MyButton extends JButton {
 
     boolean b;
     int w,h;
-    Icon no=new ImageIcon("no.png");
-    Icon yes=new ImageIcon("yes.png");
 
     public MyButton(int w,int h) {
         this.b=false;
         this.w=w;
         this.h=h;
-        this.setIcon(no);
+        this.setBackground(Color.WHITE);
         this.setPreferredSize(new Dimension(30,30));
     }
 
     public void setIcon() {
         b=!b;
         if(!b){
-            this.setIcon(no);
+            this.setBackground(Color.WHITE);
         }else{
-            this.setIcon(yes);
+            this.setBackground(Color.BLACK);
         }
 
     }
--- src/zad2/MyButtonFx.java
+++ src/zad2/MyButtonFx.java
@@ -1,42 +1,31 @@
 package zad2;
 
 import javafx.scene.control.Button;
-import javafx.scene.image.*;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class MyButtonFx extends Button {
 
+
     boolean b;
     int w,h;
-    Image yes;
-    Image no;
 
 
     public MyButtonFx(int w, int h) {
-        try{
-            no=new Image(new FileInputStream("no.png"));
-            yes=new Image(new FileInputStream("yes.png"));
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-        }
-
         this.b=false;
         this.w=w;
         this.h=h;
-        this.setGraphic(new ImageView(no));
-        this.setWidth(5);
-        this.setWidth(5);
+        this.setMaxWidth(5);
+        this.setMaxWidth(5);
+        this.setStyle("-fx-border-color: #fdff00; -fx-border-width: 2px;-fx-background-color: rgba(255,255,255,0.87)");
     }
 
     public void setIcon() {
         b=!b;
         if(!b){
-            this.setGraphic(new ImageView(no));
+            this.setStyle("-fx-border-color: #fdff00; -fx-border-width: 2px;-fx-background-color: #fbf0ff");
         }else{
-            this.setGraphic(new ImageView(yes));
+            this.setStyle("-fx-border-color: #fdff00; -fx-border-width: 2px;-fx-background-color: rgba(0,0,0,0.96)");
         }
 
     }
 }
+
